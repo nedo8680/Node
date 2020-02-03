591748bf93888938412343999b9374357a885511
Pedro Fuentes <pedrofuentes@MacBook-Pro-de-Pedro.local>
2020-02-03 16:03:12 -0500

    routing exercise with loops

--- firstExpressApp/app.js
+++ firstExpressApp/app.js
@@ -15,15 +15,41 @@ app.get("/r/:subDirectionName", (req,res)=>{
 	console.log(subName);
 	res.send(`Welcome to ${subName.subDirectionName.toUpperCase()} page`);
 });
+app.get("/speak/:subDirectionName/", (req,res)=>{
+	const subName = req.params;
+    console.log(subName);
+    if(subName.subDirectionName == "pig"){
+        res.send(`The ${subName.subDirectionName.toUpperCase()} says 'Oink'`);
+    }else if(subName.subDirectionName == "cow"){
+        res.send(`The ${subName.subDirectionName.toUpperCase()} says 'Moo'`);
+    }else if(subName.subDirectionName == "dog"){
+        res.send(`The ${subName.subDirectionName.toUpperCase()} says 'Woof Woof'`);
+    }else{
+        res.send(`Sorry, Page not found... What are you doing wuth your life`);
+    }
+});
 app.get("/r/:subDirectionName/comments/:id/:title", (req,res)=>{
 	const subName = req.params;
 	console.log(subName);
 	res.send(`Page Comments from ${subName.title.toUpperCase()}`);
 });
-
+app.get("/repeat/:word/:number", (req,res)=>{
+	const subName = req.params;
+    let output = repeatFunction(subName.word, parseInt(subName.number) );
+    console.log(output);
+	res.send(`${output.join(" ")}`);
+});
 app.get("*", (req,res)=>{
 	res.send("Page no found");
 });
 app.listen("3000", () =>{
 	console.log("Now it is serving... in http://localhost:3000");
 });
+
+const repeatFunction = (word, num) =>{
+    let newWord = [];
+    for (let i = 0; i < num; i++) {
+        newWord.push(word)
+    }
+    return newWord;
+}
